61952bdb39a9d246c57264307d0c76022330ebd7
nhkhang11 <111269703+nhkhang11@users.noreply.github.com>
2022-09-04 20:56:25 +0700

    Update answer.h
    Done

--- 08-20220824T074403Z-001/08/07_ArrayList_q5/answer.h
+++ 08-20220824T074403Z-001/08/07_ArrayList_q5/answer.h
@@ -3,10 +3,19 @@
 
 using namespace std;
 
-
 /// BEGIN  <STUDENT ANSWER>
 vector<int> updateArrayPerRange(vector<int>& nums, vector<vector<int>>& operations) {
     // TODO
-    
+    int n = operations.size();
+    int l = 0, r = 0, x = 0;
+    for (int i = 0; i<n;++i){
+        l = operations[i][0];
+        r = operations[i][1];
+        x = operations[i][2];
+        for(int j = l; j <= r;++j){
+            nums[j]+=x;
+        }
+    }
+    return nums;
 }
 /// END  <STUDENT ANSWER>
