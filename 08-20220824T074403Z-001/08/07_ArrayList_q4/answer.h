c59c1b6d135228a9a0750701a355fdb01164f65a
nhkhang11 <111269703+nhkhang11@users.noreply.github.com>
2022-09-04 20:58:17 +0700

    Update answer.h
    Done

--- 08-20220824T074403Z-001/08/07_ArrayList_q4/answer.h
+++ 08-20220824T074403Z-001/08/07_ArrayList_q4/answer.h
@@ -6,6 +6,16 @@ using namespace std;
 /// BEGIN  <STUDENT ANSWER>
 int longestSublist(vector<string>& words) {
     // STUDENT ANSWER
-    
+    int max = 0, count = 0;
+    char rec = words[0][0];
+    for(int i = 0; i < words.size();i++){
+        if (words[i][0] != rec){
+            rec = words[i][0];
+            count = 1;
+        }
+        else ++count;
+        if (count > max) max = count;
+    }
+    return max;
 }
 /// END  <STUDENT ANSWER>
