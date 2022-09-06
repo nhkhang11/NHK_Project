e0ae7dd1937c04193e9ac905e5bfe5e229824caf
nhkhang11 <111269703+nhkhang11@users.noreply.github.com>
2022-09-06 22:54:15 +0700

    done

--- 11-20220829T021618Z-001/11/11_Stack_q2/answer.h
+++ 11-20220829T021618Z-001/11/11_Stack_q2/answer.h
@@ -8,5 +8,19 @@ using namespace std;
 
 
 vector<int> nextGreater(vector<int>& arr){
-    
+    vector<int> result(arr);
+	for (int i = 0; i < arr.size(); i++)
+	{
+		for (int j = i+1; j < arr.size(); j++)
+		{
+			if (arr[j]>arr[i])
+			{
+				result[i]=arr[i];
+				break;
+			}
+			result[i]=-1;
+		}
+		result[i+1]=-1;
+	}
+	return result;
 }
