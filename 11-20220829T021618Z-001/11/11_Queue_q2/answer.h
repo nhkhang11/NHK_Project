c81252e23a9e1d97517bb2603dfddf0ab9179a58
nhkhang11 <111269703+nhkhang11@users.noreply.github.com>
2022-09-06 22:55:15 +0700

    done

--- 11-20220829T021618Z-001/11/11_Queue_q2/answer.h
+++ 11-20220829T021618Z-001/11/11_Queue_q2/answer.h
@@ -4,6 +4,39 @@
 using namespace std;
 
 	/// BEGIN  <STUDENT ANSWER>
-void bfs(vector<vector<int>> graph, int start) {
-    
+bool visited(const int &num, const vector<int> &v)
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (v[i] == num)
+			return true;
+	}
+	return false;
+}
+void bfs(vector<vector<int>> graph, int start)
+{
+	queue<int> qe;
+	vector<int> v;
+
+	qe.push(start);
+	cout << start << " ";
+	v.push_back(start);
+
+	int num;
+	while (qe.size() != 0)
+	{
+		num = qe.front();
+		qe.pop();
+		vector<int> prePush = graph[num];
+
+		for (int i = 0; i < prePush.size(); i++)
+		{
+			if (!visited(prePush[i], v))
+			{
+				qe.push(prePush[i]);
+				v.push_back(prePush[i]);
+				cout << prePush[i] << " ";
+			}
+		}
+	}
 }
