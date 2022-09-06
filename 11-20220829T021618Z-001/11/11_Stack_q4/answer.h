1f4da73403e3c497eaf451ade4662dbd39bc07c3
nhkhang11 <111269703+nhkhang11@users.noreply.github.com>
2022-09-06 22:53:03 +0700

    done

--- 11-20220829T021618Z-001/11/11_Stack_q4/answer.h
+++ 11-20220829T021618Z-001/11/11_Stack_q4/answer.h
@@ -8,5 +8,56 @@ using namespace std;
 	/// BEGIN  <STUDENT ANSWER>
 bool isValidParentheses (string s){
     /*TODO*/
-    
+    stack<char> str;
+    char c;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '[')
+        {
+            str.push(s[i]);
+        }
+        if (s[i] == ']')
+        {
+            c = str.top();
+            str.pop();
+            if (c != '[')
+            {
+                return false;
+            }
+            break;
+        }
+        if (s[i] == '(')
+        {
+            str.push(s[i]);
+        }
+        if (s[i] == ')')
+        {
+            c = str.top();
+            str.pop();
+            if (c != '(')
+            {
+                return false;
+            }
+            break;
+        }
+        if (s[i] == '{')
+        {
+            str.push(s[i]);
+        }
+        if (s[i] == '}')
+        {
+            c = str.top();
+            str.pop();
+            if (c != '{')
+            {
+                return false;
+            }
+            break;
+        }       
+    }
+    if (!str.empty())
+    {
+        return false;
+    }
+    return true;
 }
