3490b341ba91ebbfccd5700f8a17a6f836478abc
nhkhang11 <111269703+nhkhang11@users.noreply.github.com>
2022-09-06 22:54:46 +0700

    done

--- 11-20220829T021618Z-001/11/11_Stack_q1/answer.h
+++ 11-20220829T021618Z-001/11/11_Stack_q1/answer.h
@@ -8,5 +8,38 @@ using namespace std;
 	/// BEGIN  <STUDENT ANSWER>
 int baseballScore(string ops){
 	/*TODO*/
-
+	stack<int> q;
+	
+	for (int i = 0; i < ops.length(); i++)
+	{
+		if (ops[i] >= '0' && ops[i] <= '9')
+		{
+			q.push(ops[i]);
+		}
+		else if (ops[i] == 'C')
+		{
+			q.pop();
+		}
+		else if (ops[i] == 'D')
+		{
+			int db = 0;
+			db = 2*q.top();
+			q.push(db);
+		}
+		else if (ops[i] == '+')
+		{
+			int temp = q.top();
+			q.pop();
+			int temp1 = q.top();
+			q.push(temp);
+			q.push(temp1 + temp);
+		}
+	}
+	int sum = 0;
+	while (!q.empty())
+	{
+		sum+=q.top();
+		q.pop();
+	}
+	return sum;
 }
