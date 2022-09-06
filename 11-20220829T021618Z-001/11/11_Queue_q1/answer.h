beca19238019f90f8f49c0369a4d0aa9b47799a0
nhkhang11 <111269703+nhkhang11@users.noreply.github.com>
2022-09-06 22:55:43 +0700

    done

--- 11-20220829T021618Z-001/11/11_Queue_q1/answer.h
+++ 11-20220829T021618Z-001/11/11_Queue_q1/answer.h
@@ -7,5 +7,30 @@ using namespace std;
 	/// BEGIN  <STUDENT ANSWER>
 int sumOfMaxSubarray(vector<int>& nums, int k) {
     // STUDENT ANSWER
+    int sum = 0;
+    int count = 0;
+    deque<int> q;
+    for (int i = 0; i < k; i++)
+    {
+        q.push_back(nums[i]);
+        count++;
+        
+    }
+    while (count <= nums.size())
+    {
+        int max = q.front();
+        for (int i = 0; i < q.size(); i++)
+        {
+            if (q.at(i) > max)
+            {
+                max = q.at(i);
+            }
+        }
+        sum +=max;
+        count++;
+        q.pop_front();
+        q.push_back(nums[count-1]);
+    }
     
+    return sum;
 }
