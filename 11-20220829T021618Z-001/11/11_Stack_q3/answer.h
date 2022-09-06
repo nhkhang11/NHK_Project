63cc5a40df702948d566e5d62a70155d486db471
nhkhang11 <111269703+nhkhang11@users.noreply.github.com>
2022-09-06 22:53:43 +0700

    done

--- 11-20220829T021618Z-001/11/11_Stack_q3/answer.h
+++ 11-20220829T021618Z-001/11/11_Stack_q3/answer.h
@@ -9,5 +9,36 @@ using namespace std;
 	/// BEGIN  <STUDENT ANSWER>
 string removeDuplicates(string s){
     /*TODO*/
+    stack<char> str;
+    char c;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (!str.empty())
+        {
+            c = str.top();
+            if (c == s[i])
+            {
+                str.pop();
+            }else
+            {
+                str.push(s[i]);
+            }
+        }else
+        {
+            str.push(s[i]);
+        }
+    }
     
+    int size = str.size();
+    char* ptr = new char[size + 1];
+    for (int i = 0; i < size; i++)
+    {
+        ptr[size - i - 1] = str.top();
+        str.pop();
+    }
+    ptr[size] = '\0';
+    string result = ptr;
+    delete [] ptr;
+    return result;
 }
